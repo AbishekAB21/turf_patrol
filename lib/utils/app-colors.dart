d352c844d4e9c681afd71191a3ec952f36ddf258
AbishekAB21 <abishekab1805@outlook.com>
2024-10-19 09:14:04 +0530

    commit

--- lib/utils/app-colors.dart
+++ lib/utils/app-colors.dart
@@ -10,24 +10,23 @@ abstract class AppColor {
   final Color successColor;
   final Color errorColor;
 
-  AppColor({
-    required this.primaryColor,
-    required this.borderColor,
-    required this.borderColor2,
-    required this.secondaryColor,
-    required this.successColor,
-    required this.errorColor
-  });
+  AppColor(
+      {required this.primaryColor,
+      required this.borderColor,
+      required this.borderColor2,
+      required this.secondaryColor,
+      required this.successColor,
+      required this.errorColor});
 }
 
 class AppDefaultColor extends AppColor {
   AppDefaultColor()
       : super(
-            primaryColor: Colors.blueGrey.shade800,
-            borderColor: Colors.white70,
-            borderColor2: Colors.white,
-            secondaryColor: Colors.black,
-            successColor: Colors.green,
-            errorColor: Colors.red,
-            );
+          primaryColor: Colors.blueGrey.shade800,
+          borderColor: Colors.white70,
+          borderColor2: Colors.white,
+          secondaryColor: Colors.black,
+          successColor: Colors.green,
+          errorColor: Colors.red,
+        );
 }
